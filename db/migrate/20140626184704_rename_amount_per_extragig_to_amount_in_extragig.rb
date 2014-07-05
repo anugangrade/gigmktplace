e5a233f803cb77e3553a29f4953d192a5142a5ae
htp <htp@123.(none)>
2014-07-06 00:31:47 +0530

    PG syntax error fix

--- db/migrate/20140626184704_rename_amount_per_extragig_to_amount_in_extragig.rb
+++ db/migrate/20140626184704_rename_amount_per_extragig_to_amount_in_extragig.rb
@@ -1,6 +1,10 @@
 class RenameAmountPerExtragigToAmountInExtragig < ActiveRecord::Migration
   def change
   	rename_column :extragigs, :amount_per_extragig, :amount
-  	change_column :extragigs, :amount, 'integer USING CAST(amount AS integer)'
+  	if ActiveRecord::Base.connection.instance_values["config"][:adapter] == "mysql"
+  		change_column :extragigs, :amount, :integer
+  	else
+  		change_column :extragigs, :amount, 'integer USING CAST(amount AS integer)'
+  	end
   end
 end
--- db/migrate/20140626203708_modify_fields_in_transactions.rb
+++ db/migrate/20140626203708_modify_fields_in_transactions.rb
@@ -2,6 +2,10 @@ class ModifyFieldsInTransactions < ActiveRecord::Migration
   def change
   	rename_column :transactions, :amount, :extragig_id
   	rename_column :transactions, :currency, :quantity
-  	change_column :transactions, :quantity, :integer
+  	if ActiveRecord::Base.connection.instance_values["config"][:adapter] == "mysql"
+  		change_column :transactions, :quantity, :integer
+  	else
+  		change_column :transactions, :quantity, 'integer USING CAST(quantity AS integer)'
+  	end
   end
 end
