7c6030dd96cf14516307976dc4d01ddeae781abd
htp <htp@123.(none)>
2014-07-06 00:26:05 +0530

    PG syntax error fix

--- db/migrate/20140626184704_rename_amount_per_extragig_to_amount_in_extragig.rb
+++ db/migrate/20140626184704_rename_amount_per_extragig_to_amount_in_extragig.rb
@@ -1,6 +1,6 @@
 class RenameAmountPerExtragigToAmountInExtragig < ActiveRecord::Migration
   def change
   	rename_column :extragigs, :amount_per_extragig, :amount
-  	change_column :extragigs, :amount, :integer
+  	change_column :extragigs, :amount, 'integer USING CAST(column_to_change AS integer)'
   end
 end
--- db/migrate/20140702181622_remove_attributes_from_gig.rb
+++ db/migrate/20140702181622_remove_attributes_from_gig.rb
@@ -2,7 +2,7 @@ class RemoveAttributesFromGig < ActiveRecord::Migration
   def change
   	remove_column :gigs, :rating_per_order
   	remove_column :gigs, :amount_per_gig
-  	change_column :gigs, :express_boolean, :boolean
+  	change_column :gigs, :express_boolean, :boolean, default: 0
 
   	remove_attachment :videos, :video
   end
