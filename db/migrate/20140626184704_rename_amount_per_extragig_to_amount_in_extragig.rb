8e81a14f14dd9dd16a6b40d847b6bb8e4797d6f5
htp <htp@123.(none)>
2014-07-06 00:28:40 +0530

    PG syntax error fix

--- db/migrate/20140626184704_rename_amount_per_extragig_to_amount_in_extragig.rb
+++ db/migrate/20140626184704_rename_amount_per_extragig_to_amount_in_extragig.rb
@@ -1,6 +1,6 @@
 class RenameAmountPerExtragigToAmountInExtragig < ActiveRecord::Migration
   def change
   	rename_column :extragigs, :amount_per_extragig, :amount
-  	change_column :extragigs, :amount, 'integer USING CAST(column_to_change AS integer)'
+  	change_column :extragigs, :amount, 'integer USING CAST(amount AS integer)'
   end
 end
