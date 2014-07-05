d2216786eaa974dc5ada7ca163242d49227729b1
htp <htp@123.(none)>
2014-07-06 01:22:16 +0530

    change datatype of attribute in gig

--- db/migrate/20140705194601_change_category_id_type_in_gigs.rb
+++ db/migrate/20140705194601_change_category_id_type_in_gigs.rb
@@ -3,7 +3,7 @@ class ChangeCategoryIdTypeInGigs < ActiveRecord::Migration
   	if ActiveRecord::Base.connection.instance_values["config"][:adapter] == "mysql2"
   		change_column :gigs, :category_id, :integer
   	else
-  		change_column :gigs, :category_id, 'integer USING CAST(quantity AS integer)'
+  		change_column :gigs, :category_id, 'integer USING CAST(category_id AS integer)'
   	end
   end
 end
