8c2c6ff0a46e064a758467c490a8c6398ffba1d1
htp <htp@123.(none)>
2014-07-08 23:43:11 +0530

    design changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,5 +3,14 @@ module ApplicationHelper
 	def all_categories
 		return Category.all.collect {|a| [a.title]}.uniq.flatten
 	end
+
+	def flash_class(level)
+	    case level
+	        when :notice then "alert alert-info"
+	        when :success then "alert alert-success"
+	        when :error then "alert alert-error"
+	        when :alert then "alert alert-error"
+	    end
+	end
 	
 end
