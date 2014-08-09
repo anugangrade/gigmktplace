3350c792325513d31fcf1e92b378183ea075734d
123 <anu@bytelogistics.com>
2014-08-10 00:36:09 +0530

    PG compatibility work

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -2,6 +2,10 @@ class Transaction < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :gig
 
-	serialize :extragigs_quatity_id
+	serialize :extragigs_quatity_id, :if=> :mysql?
 	has_one :order_conversation
+
+	def mysql?
+		ActiveRecord::Base.connection.instance_values["config"][:adapter] == "mysql2"
+	end
 end
--- db/migrate/20140808163017_change_and_add_into_transactions.rb
+++ db/migrate/20140808163017_change_and_add_into_transactions.rb
@@ -1,7 +1,7 @@
 class ChangeAndAddIntoTransactions < ActiveRecord::Migration
   def change
-  	rename_column :transactions, :extragig_id, :extragig_ids
-  	change_column :transactions, :extragig_ids, :string, array: true, default: '{}'
+  	remove_column :transactions, :extragig_id, :integer
+  	add_column :transactions, :extragig_ids, :string, array: true, default: '{}'
   	rename_column :transactions, :quantity, :gig_quantity
   	add_column :transactions, :total_amount, :integer
   	add_column :transactions, :extragigs_quatity_id, :string, array: true, default: '{}'
